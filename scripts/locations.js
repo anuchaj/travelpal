ab60b0553381f8d3bde3c14288be6717d8cc0735
anuchaj <anuchaj50@gmail.com>
2025-02-19 20:53:43 +0100

    changes

--- scripts/locations.js
+++ scripts/locations.js
@@ -15,33 +15,43 @@ fetch(requestURL)
 function displayCard(city) {
     // Create elements to add to the document
     let card = document.createElement('section');
-    let businessName = document.createElement('p');
+    let cityName = document.createElement('p');
     let state = document.createElement('p');
     let country = document.createElement('p');
     let link = document.createElement('a');
     let description = document.createElement('p');
     let portrait = document.createElement('img');
+    let saveBtn = document.createElement('p');
+
   
     // Change the textContent property of the h2 element to contain the biz's full name
-    businessName.textContent = city.name;
+    cityName.textContent = city.name;
     state.textContent = city.state;
     country.textContent = city.country;
-    link.textContent = city.maplink;
     description.textContent = city.description;
 
     // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
     portrait.setAttribute('src', city.imageurl);
     portrait.setAttribute('alt', 'Logo of ' + city.name);
     portrait.setAttribute('loading', 'lazy');
+
+    // Build the link attributes
+    link.setAttribute("href", city.maplink);
+    link.setAttribute("target", "_blank")
+    link.textContent = "Explore " + city.name;
+
+    // Build the button attributes
+    saveBtn.setAttribute("class", "saveLocation");
+    saveBtn.textContent = "Save";
   
-    // Add/append the section(card) with the businessName element
+    // Add/append the section(card) with the cityName element
     card.appendChild(portrait);
-    card.appendChild(businessName);
+    card.appendChild(cityName);
     card.appendChild(state);
     card.appendChild(country);
-    //card.appendChild(link);
     card.appendChild(description);
-    
+    card.appendChild(link);
+    card.appendChild(saveBtn);
   
     // Add/append the existing HTML div with the cards class with the section(card)
     document.querySelector('#cards').appendChild(card);
